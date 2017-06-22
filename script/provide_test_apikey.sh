ff622c76500f43773be013dbc9905372890f86d9
bolon <fsimbolon@dyned.com>
2017-06-22 14:30:41 +0700

    add travis

---
+++ script/provide_test_apikey.sh
@@ -0,0 +1,14 @@
+KEY_PROPERTIES=$HOME"/key.properties"
+
+export KEY_PROPERTIES
+echo "Key Properties should exist at $KEY_PROPERTIES"
+
+    if [ ! -f "$KEY_PROPERTIES" ]; then
+        echo "Key Properties does not exist"
+
+        echo "Creating Key Properties file..."
+        touch $KEY_PROPERTIES
+
+        echo "Writing TEST_API_KEY to key.properties..."
+        echo "api_key_movdb=$TEST_API_KEY_ENV_VAR" >> $KEY_PROPERTIES
+    fi
