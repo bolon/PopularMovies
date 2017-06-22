d8e7e26189ba5cc6f28a30865a9d09380b401401
bolon <fsimbolon@dyned.com>
2017-06-22 15:10:39 +0700

    update travis config

--- script/provide_test_apikey.sh
+++ script/provide_test_apikey.sh
@@ -1,4 +1,4 @@
-KEY_PROPERTIES=$HOME"/key.properties"
+KEY_PROPERTIES=$TRAVIS_BUILD_DIR"/PopularMovies/key.properties"
 
 export KEY_PROPERTIES
 echo "Key Properties should exist at $KEY_PROPERTIES"
